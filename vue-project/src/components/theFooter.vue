e6069ae1d24106c52cf1578dee71bfa32a8787d0
Antonina Zakharova <zaharovaantonina01@gmail.com>
2024-01-24 01:08:18 +0100

    g

--- vue-project/src/components/theFooter.vue
+++ vue-project/src/components/theFooter.vue
@@ -2,7 +2,8 @@
     <footer>
         <div class="container">
             <div class="col-lg-12">
-                <p>Copyright © 2048 Villa Agency Co., Ltd. All rights reserved. </p>
+                <p>Copyright © 2024 Villa Agency Co., Ltd. All rights reserved. </p>
+                <p>Created by Antonina Zakharova </p>
             </div>
         </div>
     </footer>
