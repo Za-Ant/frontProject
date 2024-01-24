d511da28a2c79c78299575be28f3c1ccf79d4325
Antonina Zakharova <zaharovaantonina01@gmail.com>
2024-01-24 14:56:56 +0100

    g

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -202,7 +202,7 @@
                       <div class="info-table">
                         <ul>
                           <li>Total Flat Space <span>320 m2</span></li>
-                          <li>Floor number <span>34th</span></li>
+                          <li>Floor number <span>2</span></li>
                           <li>Number of rooms <span>6</span></li>
                           <li>Parking Available <span>Yes</span></li>
                           <li>Payment Process <span>Bank</span></li>
